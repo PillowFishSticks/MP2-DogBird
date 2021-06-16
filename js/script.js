d00b9cefb3df7ab23ca678ade4f2dc17b9d8f057
PillowFishSticks <markcpercy@gmail.com>
2021-06-16 14:49:09 +0000

    place rocket covers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
+
 // Load images
 
 var dog = new Image();
@@ -10,15 +11,20 @@ var rocketSouth = new Image();
 
 dog.src = "../images/dogrocket.png";
 bg.src = "../images/sbg.png";
-rocketNorth.src = "../images/rocketnorth.png";
-rocketSouth.src = "../images/rocketsouth.png";
+rocketNorth.src = "../images/rocketNorth.png";
+rocketSouth.src = "../images/rocketSouth.png";
+
+//variables
+var gap = 75;
+var constant = rocketNorth.height+gap;
 
 // draw images
 
 function draw(){
 
     ctx.drawImage(bg,0,0);
-    ctx.drawImage(rocketNorth,0,0);
+    ctx.drawImage(rocketNorth,100,-30);
+    ctx.drawImage(rocketSouth,100,0+constant);
 }
 
 draw();
